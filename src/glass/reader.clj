0aa741b76de88f3ef0bcf63dff979202bbc16b27
indy <kslvsunil@gmail.com>
2026-03-21 22:23:51 +0530

    Load #env values from classpath .env

--- src/glass/reader.clj
+++ src/glass/reader.clj
@@ -1,10 +1,46 @@
 (ns glass.reader
   (:require
    [aero.core :as aero]
+   [clojure.java.io :as io]
+   [clojure.string :as str]
    [integrant.core :as ig]))
 
 (def reader aero/reader)
-(def read-config aero/read-config)
+
+(defn- parse-dotenv-line
+  [line-number line]
+  (let [line (str/trim line)]
+    (cond
+      (str/blank? line) nil
+      (str/starts-with? line "#") nil
+      :else
+      (let [separator-index (str/index-of line "=")
+            key (when separator-index
+                  (subs line 0 separator-index))]
+        (when-not (and separator-index (not (str/blank? key)))
+          (throw (ex-info "Invalid .env line"
+                          {:line-number line-number
+                           :line line})))
+        [key (subs line (inc separator-index))]))))
+
+(defn- load-dotenv
+  []
+  (when-let [resource (io/resource ".env")]
+    (with-open [reader (io/reader resource)]
+      (reduce
+       (fn [dotenv [line-number line]]
+         (if-let [[key value] (parse-dotenv-line line-number line)]
+           (assoc dotenv key value)
+           dotenv))
+       {}
+       (map-indexed (fn [index line] [(inc index) line])
+                    (line-seq reader))))))
+
+(defn read-config
+  ([source]
+   (read-config source {}))
+  ([source opts]
+   (aero/read-config source (assoc opts :dotenv (load-dotenv)))))
 
 (defmethod reader 'sys/ref
   [_opts _tag value]
@@ -15,5 +51,7 @@
   (ig/refset value))
 
 (defmethod reader 'env
-  [_opts _tag key]
-  (System/getenv key))
+  [opts _tag key]
+  (let [key (str key)]
+    (or (System/getenv key)
+        (get (:dotenv opts) key))))
