f3d533ad86bf3b1d7f3a0373120c8d99b089d80d
indy <kslvsunil@gmail.com>
2026-03-26 00:06:24 +0530

    Export core Integrant API from glass.system

--- src/glass/system.clj
+++ src/glass/system.clj
@@ -1,22 +1,28 @@
 (ns glass.system
+  (:refer-clojure :exclude [ref])
   (:require
    [clojure.java.io :as io]
    [glass.reader :as reader]
    [integrant.core :as ig]))
 
+(def init ig/init)
+(def halt! ig/halt!)
+(def resume ig/resume)
+(def suspend! ig/suspend!)
+
 (def init-key ig/init-key)
 (def halt-key! ig/halt-key!)
+(def resume-key ig/resume-key)
+(def suspend-key! ig/suspend-key!)
+
+(def load-namespaces ig/load-namespaces)
+(def ref ig/ref)
+(def refset ig/refset)
 
 (defmethod init-key :default [_ x] x)
 
-(defn init
+(defn init-from-resource
   [conf-path profile]
   (-> (io/resource conf-path)
       (reader/read-config {:profile profile})
-      ig/init))
-
-(defn halt!
-  ([system]
-   (ig/halt! system))
-  ([system keys]
-   (ig/halt! system keys)))
+      init))
